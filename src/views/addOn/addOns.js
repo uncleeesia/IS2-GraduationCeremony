1c053f929743bc36d18033441b04942dced94c29
uncleeesia <weiyang1112@hotmail.com>
2023-04-23 00:01:57 +0800

    minor UI tweak

--- src/views/addOn/addOns.js
+++ src/views/addOn/addOns.js
@@ -1,6 +1,20 @@
-const buttonComponent = document.querySelector('#addOnComponents');
-fetch('src/views/addOn/addOns.html')
-.then(res=>res.text())
-.then(data=>{
-    buttonComponent.innerHTML=data;
-})
+document.addEventListener("DOMContentLoaded", function (e) {
+  const buttonComponent = document.querySelector("#addOnComponents");
+  fetch("src/views/addOn/addOns.html")
+    .then((res) => res.text())
+    .then((data) => {
+      buttonComponent.innerHTML = data;
+      const inputs = buttonComponent.querySelectorAll("input");
+      inputs.forEach((input) => {
+        input.addEventListener("click", onClick);
+      });
+    });
+});
+
+function onClick(e) {
+  e.preventDefault();
+  console.log(e.target.id);
+  //if clap do this audio
+  //else if cheer do this audio
+  //else both
+}
