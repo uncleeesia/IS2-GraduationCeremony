455b8e06e1aa065d3dbe3fce02d05f6621fbe4e1
uncleeesia <weiyang1112@hotmail.com>
2023-04-23 11:24:53 +0800

    added onclick to load the effect sound

--- src/views/addOn/addOns.js
+++ src/views/addOn/addOns.js
@@ -6,15 +6,30 @@ document.addEventListener("DOMContentLoaded", function (e) {
       buttonComponent.innerHTML = data;
       const inputs = buttonComponent.querySelectorAll("input");
       inputs.forEach((input) => {
-        input.addEventListener("click", onClick);
+        input.addEventListener("click", (e) => {
+          e.preventDefault();
+          var url;
+          switch (e.target.id) {
+            case "clapBtn":
+              url = "../src/asset/audio/among-us.wav";
+              break;
+            case "bothBtn":
+              url = "../src/asset/audio/pew.wav";
+              break;
+            case "cheerBtn":
+              url = "../src/asset/audio/spongebob-fail.wav";
+              break;
+          }
+          loadAudio(url);
+        });
       });
     });
 });
 
-function onClick(e) {
-  e.preventDefault();
-  console.log(e.target.id);
-  //if clap do this audio
-  //else if cheer do this audio
-  //else both
+function loadAudio(url) {
+  const audioContext = new AudioContext();
+  const audio = new Audio(url);
+  const track = audioContext.createMediaElementSource(audio);
+  track.connect(audioContext.destination);
+  audio.play();
 }
